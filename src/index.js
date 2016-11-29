53a9a2192fb61069f5b8bdccac56766860c0a608
mkevinevans <mkevinevans@gmail.com>
2016-11-29 13:45:28 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 const app = "I don't do much.";
 console.log(app);
+
+
+export function addMovie(movie){
+  return {
+    type: 'ADD_MOVIE',
+    title: movie.title
+  }
+}
+
+export function deleteMovie(movie){
+  return {
+    type: 'DELETE_MOVIE',
+    movie: movie
+  }
+}
+
+export function updateUsername(username){
+  return {
+    type: 'UPDATE_USERNAME',
+    username: username
+  }
+}
+
+export function resetUsername(){
+  return {
+    type: 'RESET_USERNAME',
+    username: username
+  }
+}
